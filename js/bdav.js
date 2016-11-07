d1123b5f92b0a0ffbd0f660985db4e394773ca5f
U-Brian-PC\Brian <bdav87@gmail.com>
2016-11-06 21:54:35 -0600

    updated index and bdav.js. included buttons

--- js/bdav.js
+++ js/bdav.js
@@ -1,1 +1,23 @@
-console.log("testing");
+function paperClip() {
+	$(".well p").text("Give me some paper to clip together");
+}
+
+function note() {
+	$(".well p").text("Music to my ears!");
+}
+
+function leaf() {
+	$(".well p").text("Plant a tree");
+}
+
+function gear() {
+	$(".well p").text("Gear");
+}
+
+function bell() {
+	$(".well p").text("Ding Dong!");
+}
+
+function heart() {
+	$(".well p").text("I heart you");
+}
