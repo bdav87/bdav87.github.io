3d3b92eb0f370817440d7e92e5ed1f60794b375f
U-Brian-PC\Brian <bdav87@gmail.com>
2016-12-02 14:43:49 -0600

    Removed functions from HTML and used event handling instead

--- js/bdav.js
+++ js/bdav.js
@@ -1,30 +1,38 @@
-//General JS
-function paperClip() {
+//Document ready wrapper
+$(function(){
+
+
+$("#Skills").on("click", function() {
 	$(".well p").text("Developer Skill Assessment");
 	$(".skillArea").toggle(500);
-}
+})
 
-function note() {
+$("#Music").on("click", function() {
 	$(".well p").text("Music to my ears!");
 	$("#musicArea").toggle(500);
-}
-
-function leaf() {
-	$(".well p").text("Plant a tree");
-}
-
-function gear() {
-	$(".well p").text("Gear");
-}
-
-function bell() {
-	$(".well p").text("Ding Dong!");
-}
-
-function heart() {
-	$(".well p").text("I heart you");
-}
-
-$(function(){
-$('[data-toggle="tooltip"]').tooltip({animation:true});	
 })
+
+$("#Leaf").on("click", function() {
+	$(".well p").text("Plant a tree");
+})
+
+$("#Cog").on("click", function() {
+	$(".well p").text("Gear");
+})
+
+$("#Bell").on("click",function() {
+	$(".well p").text("Ding Dong!");
+})
+
+$("#Heart").on("click", function() {
+	$(".well p").text("I heart you");
+})
+
+
+$('[data-toggle="tooltip"]').tooltip({animation:true});	
+
+
+
+//End of document ready function
+});
+
